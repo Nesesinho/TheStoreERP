44d2222d6c94a96c7bbc4fd17cd4b616c3c496cc
nese <anthony.passos8@gmail.com>
2023-12-14 05:23:57 -0300

    troca de menus agora funcionando

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,13 @@
 import { menu } from "./controllers/menuController.js";
 import { nexum } from "./controllers/nexumController.js";
+import { hm } from "./helpers/html.js";
 
 menu.change(menu.classes.clients);
+
+hm.get("#menus").querySelectorAll("li").forEach((element, index) => {
+    element.addEventListener("click", e => {
+        menu.change(menu.classes[["overview", "clients", "products"][index]]);
+    })
+});
+
 nexum.init();
